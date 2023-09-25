355ccb45640fb0eaabc5e02562d2630b7fa4ccf1
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2023-09-25 23:32:01 +0200

    Created Weather.js and Weather.css components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      logo
+      <h3>logo</h3>
       <h2>My Weather App</h2>
+      <Weather />
       <div className="container">
         <footer>
           This project was coded by {""}
@@ -14,18 +16,19 @@ export default function App() {
           >
             Ticha Maenga {""}
           </a>
-          {""} and is
+          {""} and is {""}
           <a
             href="https://github.com/TishaNaturralle/myawesome-react-weather-app"
             target="_blank rel=noopener noreferrer"
           >
             open-sourced on GitHub
           </a>{" "}
+          and {""}
           <a
             href="https://app.netlify.com/sites/voluble-cascaron-2824a8/"
             target="_blank rel=noopener noreferrer"
           >
-            and Hosted on Netlify
+            Hosted on Netlify
           </a>
         </footer>
       </div>
