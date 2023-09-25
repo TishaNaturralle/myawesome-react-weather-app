9cce5706e164950ecfa86e4ba0ac2e468025206c
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2023-09-25 23:15:01 +0200

    Added  bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       logo
-      <header className="App-header"></header>
+      <h2>My Weather App</h2>
+      <div className="container">
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://www.tisha.io"
+            target="_blank rel=noopener noreferrer"
+          >
+            Ticha Maenga {""}
+          </a>
+          {""} and is
+          <a
+            href="https://github.com/TishaNaturralle/myawesome-react-weather-app"
+            target="_blank rel=noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          <a
+            href="https://app.netlify.com/sites/voluble-cascaron-2824a8/"
+            target="_blank rel=noopener noreferrer"
+          >
+            and Hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
