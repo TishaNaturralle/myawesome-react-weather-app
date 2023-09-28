261263bc744b94d8e2fce71282f0fd801f6dcc55
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2023-09-28 04:26:31 +0200

    Imported Axios,

--- src/Weather.js
+++ src/Weather.js
@@ -1,50 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import "./Weather.css";
 
 export default function Weather() {
+  const apiKey = "73a00877081bd43422bdee0f3022beb5";
+  let formattedCity = city.split(" ").join("%20");
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${formattedCity}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+}
+if (weatherData.ready) {
   return (
     <div className="Weather">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-9">
             <input
               type="search"
-              placeholder="Enter a City.."
+              placeholder="Enter a city.."
               className="form-control"
               autoFocus="on"
+              onChange={handleCityChange}
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
-      <h1>Plettenberg Bay</h1>
-      <ul>
-        <li>Tuesday 21:29</li>
-        <li>Light rain showers</li>
-      </ul>
-      <div className="row mt-2">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png "
-              alt="Light rain showers"
-              className="float-left"
-            />
-
-            <span className="temperature">14</span>
-            <span className="unit">°C</span>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 55%</li>
-            <li>Humidity:85%</li>
-            <li>Wind:8km/hr</li>
-          </ul>
-        </div>
-      </div>
+      <WeatherInfo data={weatherData} />
+      <WeatherForecast coordinates={weatherData.coordinates} />
     </div>
   );
+} else {
+  search();
+  return "Loading...";
 }
