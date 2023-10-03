c3c2ce4619687138af92daa7a6faae5fd12fa9a0
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2023-10-03 04:51:33 +0200

    Polished up project

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,5 +1,4 @@
 import React from "react";
-import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
   console.log(props.data);
@@ -24,7 +23,9 @@ export default function WeatherForecastDay(props) {
   return (
     <div>
       <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.weather} size={36} />
+      <div className="Weather">
+        <img alt="icon" src={props.data.condition.icon_url} />
+      </div>
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
